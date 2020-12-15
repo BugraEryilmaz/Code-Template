b1c36c21de64cc7d2a0fc122a8a6095d4dd077bb
Ahmet Berker KOÇ <koc.ahmet@metu.edu.tr>
2020-12-15 19:40:14 +0300

    rotate change

--- raytracer.cpp
+++ raytracer.cpp
@@ -427,20 +427,32 @@ matrix InverseScaling(double i, double j, double k) {
 }
 
 matrix Rotate(double angle, double u, double v, double w) {
-    Vec3f vec;
-    vec.x = u;
-    vec.y = v;
-    vec.z = w;
-    vec.normalize();
+    Vec3f vecu;
+    Vec3f vecv;
+    Vec3f vecw;
+    vecu.x = u;
+    vecu.y = v;
+    vecu.z = w;
+    vecu.normalize();
+    vecv.x = -v;
+    vecv.y = u;
+    vecv.z = 0;
+    vecv.normalize();
+    vecw.x = -u * w;
+    vecw.y = -v * w;
+    vecw.z = u * u + v * v;
+    vecw.normalize();
     matrix M;
-    M.Put(0, 0, vec.x);
-    M.Put(0, 1, vec.y);
-    M.Put(0, 2, vec.z);
-    M.Put(1, 0, -vec.y);
-    M.Put(1, 1, vec.x);
-    M.Put(2, 0, -vec.x * vec.z);
-    M.Put(2, 1, -vec.y * vec.z);
-    M.Put(2, 2, vec.x * vec.x + vec.y * vec.y);
+    M.Put(0, 0, vecu.x);
+    M.Put(0, 1, vecu.y);
+    M.Put(0, 2, vecu.z);
+    M.Put(1, 0, vecv.x);
+    M.Put(1, 1, vecv.y);
+    M.Put(1, 2, vecv.z);
+    M.Put(2, 0, vecw.x);
+    M.Put(2, 1, vecw.y);
+    M.Put(2, 2, vecw.z);
+    M.Put(3, 3, 1);
     matrix R;
     R.Put(0, 0, 1);
     R.Put(3, 3, 1);
